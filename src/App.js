786338436487e06b60a17705bc179cefbc76bbda
Dizlu <golmarcin@gmail.com>
2017-05-03 20:11:00 +0200

    improve css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css'
 
 class App extends Component {
   componentDidMount() {
-    this.interval = setInterval(() => this.props.boardLifecycle(this.props.board.board), 100)
+    this.interval = setInterval(() => this.props.boardLifecycle(this.props.board.board), 300)
   }
 
   componentWillUnmount() {
